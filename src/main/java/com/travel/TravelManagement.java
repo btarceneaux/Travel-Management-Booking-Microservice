cb3ac98fcfac3cacd4ea11f2cc31f72c16c5dd62
Burton Arceneaux <btarceneaux@gmail.com>
2023-09-17 18:43:59 -0700

    Initial commit.

---
+++ src/main/java/com/travel/TravelManagement.java
@@ -0,0 +1,15 @@
+package com.travel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelManagement
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TravelManagement.class, args);
+    }
+
+}
