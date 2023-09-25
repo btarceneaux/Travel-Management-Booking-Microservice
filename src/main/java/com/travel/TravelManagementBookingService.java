0fdae4ba5ae1bbf07c412984a26e639f6a3ba624
Burton Arceneaux <btarceneaux@gmail.com>
2023-09-24 19:40:57 -0700

    Got everything working with the Eureka server.

--- src/main/java/com/travel/TravelManagementBookingService.java
+++ src/main/java/com/travel/TravelManagementBookingService.java
@@ -4,15 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.travel.bean")
 @EnableJpaRepositories(basePackages = "com.travel.repository")
-public class TravelManagement
+@EnableDiscoveryClient
+public class TravelManagementBookingService
 {
     public static void main(String[] args)
     {
-        SpringApplication.run(TravelManagement.class, args);
+        SpringApplication.run(TravelManagementBookingService.class, args);
     }
 
 }
