d8d63f55414238bab44b74de1403924a71ff2fac
Burton Arceneaux <btarceneaux@gmail.com>
2023-11-05 17:28:00 -0800

    Got rid of duplicate junit.

--- src/test/java/com/travel/service/CustomerServiceTest.java
+++ src/test/java/com/travel/service/CustomerServiceTest.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.travel.bean.Customer;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
-
-import java.util.List;
 import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
